8ab2b499d36d90dca3f9139fd061e08c0c627800
mastroddi <mastroddi@127b15f6-8070-407b-945c-913c06b37495>
2011-08-22 18:30:00 +0000

    git-svn-id: https://svn.code.sf.net/p/beebeep/code@65 127b15f6-8070-407b-945c-913c06b37495

--- Version.h
+++ Version.h
@@ -25,7 +25,7 @@
 #define BEEBEEP_VERSION_H
 
 const char* BEEBEEP_NAME = "BeeBEEP";
-const char* BEEBEEP_VERSION = "0.4.62";
+const char* BEEBEEP_VERSION = "0.4.65";
 
 
 #endif // BEEBEEP_VERSION_H
