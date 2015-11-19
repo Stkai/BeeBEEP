9fba3f961a17f42719768a1b705f26547e2938c0
mastroddi <mastroddi@127b15f6-8070-407b-945c-913c06b37495>
2015-11-19 16:33:41 +0000

    git-svn-id: https://svn.code.sf.net/p/beebeep/code@574 127b15f6-8070-407b-945c-913c06b37495

--- src/Version.h
+++ src/Version.h
@@ -39,10 +39,10 @@ const char* BEEBEEP_HELP_WEBSITE = "/help.php";
 const char* BEEBEEP_LANGUAGE_WEBSITE = "/language.php";
 const char* BEEBEEP_CHECK_VERSION_WEBSITE = "/checkversion.php";
 const char* BEEBEEP_PAGE_ON_FACEBOOK = "https://www.facebook.com/beebeeplanmessenger";
-const char* BEEBEEP_VERSION = "3.0.2";
+const char* BEEBEEP_VERSION = "3.0.3";
 const int BEEBEEP_PROTO_VERSION = 62;
 const int BEEBEEP_SETTINGS_VERSION = 5;
-const int BEEBEEP_BUILD = 569;
+const int BEEBEEP_BUILD = 574;
 
 #endif // BEEBEEP_VERSION_H
 
