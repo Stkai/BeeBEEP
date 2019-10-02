b9ee3bdac81ee6ff7f3c0b6807f4fcae0c810cd2
mastroddi <mastroddi@127b15f6-8070-407b-945c-913c06b37495>
2019-10-02 16:26:40 +0000

    git-svn-id: https://svn.code.sf.net/p/beebeep/code@1278 127b15f6-8070-407b-945c-913c06b37495

--- os2_compile.bat
+++
@@ -1,5 +0,0 @@
-echo BeeBEEP fro OS/2 qt 4
-set MAKESHELL="cmd.exe"
-qmake -o Makefile beebeep-desktop.pro
-make all
-echo "Done."
--- os2_compile_qt5.bat
+++
@@ -1,5 +0,0 @@
-echo BeeBEEP fro OS/2 qt 5
-set MAKESHELL="cmd.exe"
-qmake-qt5 -o Makefile beebeep-desktop.pro
-make all
-echo "Done."
