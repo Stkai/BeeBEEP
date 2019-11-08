b05de3aefdc3190951212117e45b016230c920e0
mastroddi <mastroddi@127b15f6-8070-407b-945c-913c06b37495>
2019-11-08 09:37:36 +0000

    Released BeeBEEP for Raspberry PI with voice messages available (Qt 5 version).
    git-svn-id: https://svn.code.sf.net/p/beebeep/code@1355 127b15f6-8070-407b-945c-913c06b37495

---
+++ scripts/deploy_linux_arm64_qt5_package.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# This file is part of BeeBEEP.
+#
+# BeeBEEP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# BeeBEEP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BeeBEEP.  If not, see <http:#www.gnu.org/licenses/>.
+#
+# Author: Marco Mastroddi <marco.mastroddi(AT)gmail.com>
+#
+# $Id: deploy_linux_amd64_qt4_package.txt 616 2015-12-14 17:37:25Z mastroddi $
+#
+######################################################################
+
+BEEBEEP_QT_VERSION=qt5
+BEEBEEP_ARCH_TYPE=arm64
+
+. ./inc_linux_package.txt
